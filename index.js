280b6834fe8c0f692c72004c85e469a21ea373b3
LisPseu <151450347+LisPseu@users.noreply.github.com>
2024-12-01 20:21:43 +0300

    italy_restorant_with_animation

---
+++ index.js
@@ -0,0 +1,7 @@
+gsap.to(".one", {delay: 1, duration: 2, x:-400, ease:"power2.inOut", opacity: 0});
+gsap.from(".two", {delay: 1.2, duration: 2, x:-400, ease:"power2.inOut", opacity: 0});
+gsap.from(".header", {delay: 2.5, duration: 2, y:100, opacity: 0});
+gsap.from('.links', { duration: 2, delay: 2.4, y: '10vw', ease: 'power2.in', opacity: 0 });
+gsap.from('.mobile', { duration: 2, delay: 2.4, y: '10vw', ease: 'power2.in', opacity: 0 });
+gsap.from('h1', { duration: 2, delay: 1.5, x: '-100vw', ease: 'power2.in', opacity: 0 });
+gsap.fromTo('.btn', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 4.3, opacity: 1, scale: 1, rotation: 0 })
